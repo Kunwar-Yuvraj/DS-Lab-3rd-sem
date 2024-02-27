3dc6d2e2067f95eefa0ef20eff5714efdd15de9c
Kunwar Yuvraj <63793259+Kunwar-Yuvraj@users.noreply.github.com>
2024-02-27 13:46:59 +0530

    Update 9a.c (done)

--- Lab 9/9a.c
+++ Lab 9/9a.c
@@ -3,160 +3,220 @@ Develop a menu driven program to convert infix expressions to postfix
 expression and evaluate the postfix expression.
 */
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-#define MAX_SIZE 100
 
-typedef struct {
-    char stack[MAX_SIZE];
+#define MAX_SIZE 25
+
+
+typedef struct{
+    char exp[MAX_SIZE];
     int top;
-} op_stack;
+} Stack;
 
-void init_op_stack(op_stack *s) {
-    s->top = -1;
+
+Stack *initialize(){
+    Stack *stack = (Stack *)malloc(sizeof(Stack));
+    if (!stack){
+        printf("Memory allocation for stack failed!\n");
+        exit(1);
+    }
+    stack->top = -1;
+
+    return stack;
 }
 
-void push_operator(op_stack *s, char op) {
-    s->stack[++s->top] = op;
+
+void push(Stack *stack, char c){
+    stack->exp[++stack->top] = c;
 }
 
-char pop_operator(op_stack *s) {
-    return s->stack[s->top--];
+
+int pop(Stack *stack){
+    if (stack->top == -1)
+        return -1;
+    return stack->exp[stack->top--];
 }
 
-char peek_operator(op_stack *s) {
-    return s->stack[s->top];
+int peek(Stack *stack){
+    if (stack->top == -1)
+        return -1;
+    return stack->exp[stack->top];
 }
 
-int is_operator(char c) {
-    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
+
+int is_empty(Stack *stack){
+    if (stack->top == -1){
+        return 1;
+    }
+    return 0;
 }
 
-int get_precedence(char op) {
-    switch (op) {
-        case '+':
-        case '-':
-            return 1;
+
+int is_op(char c){
+    switch (c){
+        case '^':
         case '*':
         case '/':
-            return 2;
-        case '^':
-            return 3;
-        default:
-            return -1;
+        case '+':
+        case '-': return 1;
+        default: return 0;
     }
 }
 
-void infix_to_postfix(char infix[], char postfix[]) {
-    op_stack op_stack;
-    init_op_stack(&op_stack);
 
-    int i, j;
-    for (i = 0, j = 0; infix[i] != '\0'; i++) {
-        if (
-            (infix[i] >= 'a' && infix[i] <= 'z') 
-            || (infix[i] >= 'A' && infix[i] <= 'Z')
-        ) {
-            postfix[j++] = infix[i];
-        }
-        else if (infix[i] == '(') {
-            push_operator(&op_stack, infix[i]);
-        }
-        else if (infix[i] == ')') {
-            while (peek_operator(&op_stack) != '(') {
-                postfix[j++] = pop_operator(&op_stack);
-            }
-            pop_operator(&op_stack);
-        }
-        else if (is_operator(infix[i])) {
-            while (
-                get_precedence(infix[i]) <= get_precedence(peek_operator(&op_stack))
-            ) {
-                postfix[j++] = pop_operator(&op_stack);
-            }
-            push_operator(&op_stack, infix[i]);
-        }
+int precedence(char op){
+    switch (op){
+        case '^': return 3;
+        case '/':
+        case '*': return 2;
+        case '+':
+        case '-': return 1;
+        default: return 0;
     }
-
-    while (op_stack.top != -1) {
-        postfix[j++] = pop_operator(&op_stack);
-    }
-
-    postfix[j] = '\0';
 }
 
-int evaluate_postfix(char postfix[]) {
-    op_stack operand_stack;
-    init_op_stack(&operand_stack);
+
+char ass(char op){
+    if (op == '^')
+        return 'R';
+    return 'L';
+}
+
+
+void convert(char infix[], Stack *postfix){
+    Stack *op = initialize();
 
     int i;
-    for (i = 0; postfix[i] != '\0'; i++) {
-        if (postfix[i] >= '0' && postfix[i] <= '9') {
-            push_operator(&operand_stack, postfix[i] - '0');
-        }
-        else {
-            int op2 = pop_operator(&operand_stack);
-            int op1 = pop_operator(&operand_stack);
+    for (i = 0; infix[i] != '\0'; i++){
+        char c = infix[i];
 
-            switch (postfix[i]) {
-                case '+':
-                    push_operator(&operand_stack, op1 + op2);
-                    break;
-                case '-':
-                    push_operator(&operand_stack, op1 - op2);
-                    break;
-                case '*':
-                    push_operator(&operand_stack, op1 * op2);
-                    break;
-                case '/':
-                    push_operator(&operand_stack, op1 / op2);
-                    break;
+        if (c == '('){
+            push(op, c);
+        }
+        else if (c == ')'){
+            while (peek(op) != '('){
+                push(postfix, pop(op));
+            }
+            pop(op);
+
+        }
+        else if (is_op(c)){
+            if (op->top == -1){
+                push(op, c);
+            }
+            else{
+                if (precedence(c) > precedence(peek(op))){
+                    push(op, c);
+                }
+                else{
+                    while (precedence(c) <= precedence(peek(op))){
+                        push(postfix, pop(op));
+                    }
+                    push(op, c);
+                }
+            }
+        }
+        else{
+            push(postfix, c);
+        }
+    }
+    while (!is_empty(op)){
+        push(postfix, pop(op));
+    }
+    push(postfix, '\0');
+    
+    free(op);
+}
+
+
+float evaluate_postfix(char postfix[]){
+    int i;
+    Stack *result = initialize();
+
+    for (i = 0; postfix[i] != '\0'; i++){
+        char c = postfix[i];
+
+        if (!is_op(c)){
+            push(result, c);
+        }
+        else{
+            int a = pop(result) - '0';
+            int b = pop(result) - '0';
+            switch (c){
                 case '^':
-                    push_operator(&operand_stack, (int)pow(op1, op2));
+                    push(result, pow(a, b) + '0');
+                    break;
+
+                case '*':
+                    push(result, (a*b) + '0');
+                    break;
+                
+                case '/':
+                    push(result, (a/b) + '0');
+                    break;
+
+                case '+':
+                    push(result, (a+b) + '0');
+                    break;
+
+                case '-':
+                    push(result, (a-b) + '0');
+                    break;
+                
+                default:
                     break;
             }
         }
     }
-
-    return pop_operator(&operand_stack);
+    
+    return pop(result) - '0';
 }
 
-int main() {
-    char infix[MAX_SIZE];
-    char postfix[MAX_SIZE];
 
-    printf("\nMenu:\n");
-    printf("1. Convert Infix to Postfix\n");
-    printf("2. Evaluate Postfix Expression\n");
-    printf("3. Exit\n");
+int main(){
+    char infix[MAX_SIZE];
+
+    printf("\nMAIN MENU :>\n\n");
+    printf("1. Infix to Postfix\n");
+    printf("2. Evaluate Postfix\n");
+    printf("0. Exit\n");
 
     int choice;
-    while (1) {
-        printf("Enter your choice (1/2/3): ");
+    do{
+    Stack *postfix = initialize();
+        printf("\nEnter your choice: ");
         scanf("%d", &choice);
 
-        switch (choice) {
-            case 1:
-                printf("Enter the infix expression: ");
+        switch (choice){
+            case 1:{
+                printf("Enter infix expression: ");
                 scanf("%s", infix);
-                infix_to_postfix(infix, postfix);
-                printf("Postfix expression: %s\n", postfix);
+                convert(infix, postfix);
+                printf("Postfix: %s\n", postfix->exp);
+                free(postfix);
                 break;
-            case 2:
-                printf("Enter the postfix expression: ");
-                scanf("%s", postfix);
-                int result = evaluate_postfix(postfix);
-                printf("Result: %d\n", result);
+            }
+            case 2:{
+                printf("Enter postfix to evaluate: ");
+                scanf("%s", postfix->exp);
+                printf("Result: %f\n", evaluate_postfix(postfix->exp));
+                free(postfix);
                 break;
-            case 3:
+            }
+            case 0:{
                 printf("Exiting...\n");
-                exit(0);
-            default:
-                printf("Invalid choice. Please enter a valid option.\n");
-        }
-    }
+                free(postfix);
+                break;
+            }
+            default:{
+                printf("Invalid choice!\n");
+            }
+        } 
+    } while (choice != 0);
 
     return 0;
 }
